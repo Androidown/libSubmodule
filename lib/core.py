8d93770eea21430464c6a56e02c87773e99af05b
weiwei <wei.wei@proinnova.com.cn>
2021-01-20 13:54:55 +0800

    change libsub in test

--- lib/core.py
+++ lib/core.py
@@ -2,4 +2,4 @@
 
 def core_func():
 	print("I'm the core function.")
-
+## make some change here
--- tools/byte_util.py
+++ tools/byte_util.py
@@ -2,5 +2,4 @@
 
 def to_int(bytes):
 	print(f"got bytes {bytes}")
-	print(f"got another update.")
 	return int.from_bytes(bytes, 'little')
